566eaa8cf8f3c1846208037a2d7a5698816c6401
thor56 <38258444+thor56@users.noreply.github.com>
2023-05-18 14:38:34 +0530

    Changed the Navbar to responsive design and adapted to entire site
    Converted the project to Jekyll based. Follow jekyll installation on their site to setup and run.
    
    known issues :
    - footer bug in few pages
    - Some pages has width beyond the screen for smaller screens (weird layout issues)
    - code boxes on right side of JS exercise page needs to be aligned

---
+++ _site/code_layouts/test.js
@@ -0,0 +1,6 @@
+function openNav() {
+    // document.getElementsByClassName("sidebar").classList.toggle("nav-toggle");
+    // document.getElementById("main").classList.toggle("nav-toggle");
+    console.log(document.getElementById('mySidebar').classList.toggle('nav-toggle'));
+    console.log(document.getElementById('main').classList.toggle('nav-toggle'));
+}
